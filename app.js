7137195800c585cf28242cb94d1df00782e1b52d
Faith Goins <faithtoriangoins@gmail.com>
2023-03-09 10:45:48 -0600

    initalize and declared variables

--- app.js
+++ app.js
@@ -4,7 +4,32 @@ console.log(
 );
 
 // Exercise 1
+const firstname = "faith"; //can not declare a const without initializing
+let lastname;
+var age;
 
 // Exercise 2
+firstname = "Faithtorian";
+lastname = "Goins";
+age =  23;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+//excersize 4 
+let price = 19.99;
+let isOnSale =false;
+let salePercantagev= 15;
+let stock = 0;
+let instock = false;
+let selectedSize = "m";
+
+//excersize 5 
+let title = "name of the wind"
+let author = "Patrick Rothfuss"
+let pageCount = 722;
+let bookMark = 456;
+let hasRead = true;
+
